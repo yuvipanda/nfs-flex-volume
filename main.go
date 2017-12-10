29fc94d6ff1ea8376eb62e3728504e103b814247
yuvipanda <yuvipanda@gmail.com>
2017-12-10 10:04:00 -0800

    Refactor mounting code + JSON parsing into its own type
    Should make unit testing easier too!

--- main.go
+++ main.go
@@ -33,6 +33,92 @@ func makeResponse(status, message string) *Response {
 	}
 }
 
+type VolumeSource struct {
+	MountOptions      string `json:"mountOptions"`
+	SubPath           string `json:"subPath"`
+	Share             string `json:"share"`
+	CreateIfNecessary string `json:"createIfNecessary"`
+	CreateMode        string `json:"createMode"`
+}
+
+func (vs *VolumeSource) sortedMountOptions() string {
+	opts := strings.Split(vs.MountOptions, ",")
+	sort.Strings(opts)
+	return strings.Join(opts, ",")
+}
+
+func (vs *VolumeSource) mountPath() string {
+	return fmt.Sprintf("/mnt/nfsflexvolume/%s/options/%s", vs.Share, vs.sortedMountOptions())
+
+}
+
+func (vs *VolumeSource) srcPath() string {
+	if vs.SubPath != "" {
+		return path.Join(vs.mountPath(), vs.SubPath)
+	} else {
+		return vs.mountPath()
+	}
+}
+
+func (vs *VolumeSource) Mount() *Response {
+	if isStaleMount(vs.mountPath()) {
+		unmountCmd := exec.Command("umount", vs.mountPath())
+		out, err := unmountCmd.CombinedOutput()
+		if err != nil {
+			return makeResponse("Failure", fmt.Sprintf("Could not unmount stale mount %s %s: %s", vs.mountPath(), err.Error(), out))
+		}
+	}
+
+	if !isMountPoint(vs.mountPath()) {
+		err := os.MkdirAll(vs.mountPath(), 0755)
+
+		if err != nil {
+			return makeResponse("Failure", fmt.Sprintf("Could not make mountPath %s: %s", vs.mountPath(), err.Error()))
+		}
+		mountCmd := exec.Command("mount", "-t", "nfs4", vs.Share, vs.mountPath(), "-o", vs.sortedMountOptions())
+		out, err := mountCmd.CombinedOutput()
+		if err != nil {
+			return makeResponse("Failure", fmt.Sprintf("Could not mount %s: %s", err.Error(), out))
+		}
+	}
+	return nil
+}
+
+func (vs *VolumeSource) EnsureSubPath() *Response {
+	if vs.CreateIfNecessary == "true" {
+		createModeUint64, err := strconv.ParseUint(vs.CreateMode, 0, 32)
+		createMode := os.FileMode(createModeUint64)
+		err = os.MkdirAll(vs.srcPath(), createMode)
+		if err != nil {
+			return makeResponse("Failure", fmt.Sprintf("Could not create subPath: %s", err.Error()))
+		}
+	} else {
+		_, err := os.Stat(vs.srcPath())
+		if err != nil {
+			return makeResponse("Failure", fmt.Sprintf("Could not find path %s to be mounted: %s", vs.srcPath(), err.Error()))
+		}
+	}
+	return nil
+}
+
+func (vs *VolumeSource) EnsureSymlink(target string) *Response {
+	// Now we rmdir the target, and then make a symlink to it!
+	err := os.Remove(target)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return makeResponse("Failure", fmt.Sprintf("Could not remove target %s before symlink: %s", target, err.Error()))
+		}
+	}
+
+	// Handle case where the target already exists, because the pod has restarted in the meantime!
+	err = os.Symlink(vs.srcPath(), target)
+
+	if err != nil {
+		return makeResponse("Failure", fmt.Sprintf("Could not symlink %s to %s: %s", vs.srcPath(), target, err.Error()))
+	}
+	return nil
+}
+
 /// Return status
 func Init() *Response {
 	resp := makeResponse("Success", "No Initialization required")
@@ -66,69 +152,21 @@ func isMountPoint(path string) bool {
 
 /// If NFS hasn't been mounted yet, mount!
 /// If mounted, bind mount to appropriate place.
-func Mount(target string, options map[string]string) *Response {
-	opts := strings.Split(options["mountOptions"], ",")
-	sort.Strings(opts)
-	sortedOpts := strings.Join(opts, ",")
+func Mount(target string, vs *VolumeSource) *Response {
 
-	subPath := options["subPath"]
-	createIfNecessary := options["createIfNecessary"] == "true"
-	createModeUint64, err := strconv.ParseUint(options["createMode"], 0, 32)
-	createMode := os.FileMode(createModeUint64)
-
-	sharePath := options["share"]
-	//createUid := strconv.Atoi(options["createUid"])
-	//createGid := strconv.Atoi(options["createGid"])
-
-	mountPath := fmt.Sprintf("/mnt/nfsflexvolume/%s/options/%s", sharePath, sortedOpts)
-
-	if isStaleMount(mountPath) {
-		unmountCmd := exec.Command("umount", mountPath)
-		out, err := unmountCmd.CombinedOutput()
-		if err != nil {
-			return makeResponse("Failure", fmt.Sprintf("Could not unmount stale mount %s %s: %s", mountPath, err.Error(), out))
-		}
+	resp := vs.Mount()
+	if resp != nil {
+		return resp
 	}
 
-	if !isMountPoint(mountPath) {
-		err := os.MkdirAll(mountPath, 0755)
-
-		if err != nil {
-			return makeResponse("Failure", fmt.Sprintf("Could not make mountPath %s: %s", mountPath, err.Error()))
-		}
-		mountCmd := exec.Command("mount", "-t", "nfs4", sharePath, mountPath, "-o", sortedOpts)
-		out, err := mountCmd.CombinedOutput()
-		if err != nil {
-			return makeResponse("Failure", fmt.Sprintf("Could not mount %s: %s", err.Error(), out))
-		}
+	resp = vs.EnsureSubPath()
+	if resp != nil {
+		return resp
 	}
 
-	srcPath := path.Join(mountPath, subPath)
-
-	if createIfNecessary {
-		err := os.MkdirAll(srcPath, createMode)
-		if err != nil {
-			return makeResponse("Failure", fmt.Sprintf("Could not create subPath: %s", err.Error()))
-		}
-	} else {
-		_, err := os.Stat(srcPath)
-		if err != nil {
-			return makeResponse("Failure", fmt.Sprintf("Could not find path %s to be mounted: %s", srcPath, err.Error()))
-		}
-	}
-
-	// Now we rmdir the target, and then make a symlink to it!
-	err = os.Remove(target)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return makeResponse("Failure", fmt.Sprintf("Could not remove target %s before symlink: %s", target, err.Error()))
-		}
-	}
-
-	err = os.Symlink(srcPath, target)
-
-	if err != nil {
-		return makeResponse("Failure", fmt.Sprintf("Could not symlink %s to %s: %s", srcPath, target, err.Error()))
+	resp = vs.EnsureSymlink(target)
+	if resp != nil {
+		return resp
 	}
 
 	return makeResponse("Success", "Mount completed!")
@@ -148,9 +186,9 @@ func main() {
 		Init().printJson()
 	case "mount":
 		optsString := os.Args[3]
-		opts := make(map[string]string)
+		opts := VolumeSource{}
 		json.Unmarshal([]byte(optsString), &opts)
-		Mount(os.Args[2], opts).printJson()
+		Mount(os.Args[2], &opts).printJson()
 	case "unmount":
 		Unmount(os.Args[2]).printJson()
 	default:
